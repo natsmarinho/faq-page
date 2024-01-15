cb26267a844c212b385ce261bbaa6d175c696930
natsm <natsmarinho@gmail.com>
2024-01-15 16:10:49 -0300

    style: Elementos esteticos adicionados

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const question = document.querySelectorAll(".question");
+const faqItems = document.querySelectorAll(".faq-items")
 const answer = document.querySelectorAll(".answer");
 const arrow = document.querySelectorAll("svg");
 
 question.forEach((faqQuestion, index) => {
     faqQuestion.addEventListener("click", () => {
+        faqItems[index].classList.toggle("bold");
         answer[index].classList.toggle("active");
-        arrow[index].classList.toggle("active")
+        arrow[index].classList.toggle("active");
     });
 });
